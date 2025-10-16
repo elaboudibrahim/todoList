007fedc474eb3454ee097352b2e8a5c1e62b0ae0
brahim.elaboudi <brahimelaboudi1@gmail.com>
2025-10-16 09:53:24 +0100

    Configuration des routes dans l app

--- toDoList-ui/src/App.vue
+++ toDoList-ui/src/App.vue
@@ -1,61 +1,10 @@
 <script setup>
-import { ref } from "vue";
-import { Button } from '@/components/ui/button'
 
-const todos = ref([
-  {title :"Tache 1", completed : true , date:null},
-  {title :"Tache 2", completed : false , date:null},
-  {title :"Tache 3", completed : false , date:null}
-]);
-
-const newTodo = ref('');
-
-const addTodo=()=>{
-  event.preventDefault()
-  todos.value.push({
-    title : newTodo,
-    completed : false,
-    date : Date.now()
-  });
-  newTodo.value = '';
-  console.log(todos)
-}
-</script>
-<template class="container py-8">
-    <div class="container mx-auto py-8 px-4 max-w-2xl">
-
-  <div v-if="todos.length === 0"> vous n'avez pas tache à faire .</div>
-  <div v-else > 
-    <div>vous avez {{todos.length}} tahces à faire .</div>
-    <ul>
-        <li 
-          v-for = "todo in todos"
-         :key= "todo.date"
-          :class =" {completed : todo.completed}">
-        <input type="checkbox" v-model = "todo.completed"/>
-         {{todo}}
-        </li>
-    </ul>  
-  </div>
-  <form action="Post" @Submit.prevent="saveTodo">
-    <fieldSet>
-          <legend>Informations personnelles</legend>
-          <input v-model = "newTodo"
-            type="text"
-            placeholder="Tache à effectuer">
-          {{newTodo}}
-          </input>
-          <Button variant="default"
-          :disabled = "newTodo.length === 0" 
-          @click="addTodo">Ajouter </Button>
-    </fieldSet>  
-  </form>
-</div>
-
+import TasksView from './components/views/tasks/TasksView.vue';
+    </script>
+<template>
+    <RouterView />
 </template>
 <style scoped>
-.completed {
-  opacity : .5;
-  text-decoration : line-through;
-}
+
 </style>
--- toDoList-ui/src/router/index.js
+++ toDoList-ui/src/router/index.js
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import TasksView from '@/components/views/tasks/TasksView.vue'
+import { Component } from 'lucide-vue-next'
+import AuthPage from '@/pages/AuthPage.vue'
+import Home from '@/pages/Home.vue'
+import TaskDetailView from '@/components/views/tasks/TaskDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', name: 'default', component: Home },
+    { path: '/login', name: 'login', component: AuthPage },
+    { path: '/home', name: 'Home', component: Home,
+      children: [
+        {path: "/home" , component: TasksView},
+        {path: "/task/edit" , component: TaskDetailView},
+        {path: "/task/edit/:id",  name: 'TaskEdit',  component: TaskDetailView}
+      ]
+    },
+    
+
+  ],
 })
 
 export default router
