03908f67457aa8e3f0c958595ee8e0a3d006c294
brahim.elaboudi <brahimelaboudi1@gmail.com>
2025-10-16 09:47:25 +0100

    creation des pages home et auhentifiaction

---
+++ toDoList-ui/src/pages/AuthPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import LoginForm from '@/components/views/auth/LoginForm.vue';
+import Register from '@/components/views/auth/Register.vue';
+
+</script>
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900 p-6">
+    <div class="w-full max-w-md bg-white dark:bg-gray-800 rounded-2xl shadow-lg p-6 space-y-4">
+      <h1 class="text-xl font-semibold text-center text-gray-800 dark:text-gray-100">
+        Connexion / Inscription
+      </h1>
+
+      <LoginForm />
+      <Register />
+
+    </div>
+  </div>
+</template>
---
+++ toDoList-ui/src/pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Sidebar from '@/components/views/sidebar.vue';
+import TasksView from '@/components/views/tasks/TasksView.vue';
+
+</script>
+
+
+<template>
+  <div class="flex min-h-screen">
+    <aside class="w-64 bg-white dark:bg-gray-800 p-4 shadow-md">
+      <Sidebar />
+    </aside>
+
+    <main class="flex-1 p-8">
+      <div class="max-w-3xl mx-auto space-y-6">
+        <RouterView />
+      </div>
+    </main>
+  </div>
+</template>
