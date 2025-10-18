4d769600f35d54fedf094f0a6aadf78195b8018f
brahim.elaboudi <brahimelaboudi1@gmail.com>
2025-10-18 03:30:27 +0100

    creation des factories

--- toDoList-core/database/factories/UserFactory.php
+++ toDoList-core/database/factories/UserFactory.php
@@ -4,25 +4,21 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-
+use App\Models\User;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+class UserFactory extends Factory{
     public function definition()
     {
         return [
-            'name' => fake()->name(),
+            'full_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'phone_number'=> $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'image' => $this->faker->imageUrl(200, 200),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10)
         ];
     }
 
@@ -33,8 +29,8 @@ class UserFactory extends Factory
      */
     public function unverified()
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+      //  return $this->state(fn (array $attributes) => [
+    //        'email_verified_at' => null,
+       // ]);
     }
 }
