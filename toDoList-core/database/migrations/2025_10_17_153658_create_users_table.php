c8e5b76d7c1088da179b0de931d4573feb0c642c
brahim.elaboudi <brahimelaboudi1@gmail.com>
2025-10-18 03:30:43 +0100

    creations des migrations

--- toDoList-core/database/migrations/2025_10_17_153658_create_users_table.php
+++ toDoList-core/database/migrations/2025_10_17_153658_create_users_table.php
@@ -15,12 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('full_name');            
+            $table->string("email")->unique();
+            $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
+            $table->string("password");
+            $table->string('image')->nullable();
             $table->timestamps();
+            $table->rememberToken();
         });
     }
 
@@ -34,3 +36,8 @@ return new class extends Migration
         Schema::dropIfExists('users');
     }
 };
+
+
+/**User::factory()
+    ->has(Task::factory()->count(5)) // 👈 crée 5 tâches liées à cet utilisateur
+    ->create(); */
